ececbcf6009255e6a4dd4bc5a34cdbee633b9517
HuynhNam22 <honamhuynh22@gmail>
2026-06-05 20:22:43 +0700

    Modify run script for backend

--- backend/run.ps1
+++ backend/run.ps1
@@ -1,5 +1,5 @@
 $env:KMP_DUPLICATE_LIB_OK='TRUE'
-$env:PYTHONPATH=(Resolve-Path "$PSScriptRoot\..\.venv\Lib\site-packages").Path
+$env:PYTHONPATH=(Resolve-Path "$PSScriptRoot\.venv\Lib\site-packages").Path
 cd $PSScriptRoot
 $pythonPath = "$env:LOCALAPPDATA\Programs\Python\Python312\python.exe"
 & $pythonPath -m uvicorn main:app --reload
